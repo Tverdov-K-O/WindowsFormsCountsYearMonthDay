fdf88758c4fce7af4aa3b57789e61d645efe5f2f
tverdov.konstantin tverdov.konstantin <konstantin08081997@mail.ru>
2021-11-05 21:39:23 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace WindowsFormsCountsYearMonthDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.maskedTextBoxDATE1 = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBoxDATE2 = new System.Windows.Forms.MaskedTextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxDATE1
+            // 
+            this.maskedTextBoxDATE1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.maskedTextBoxDATE1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBoxDATE1.Location = new System.Drawing.Point(94, 27);
+            this.maskedTextBoxDATE1.Mask = "00/00/0000";
+            this.maskedTextBoxDATE1.Name = "maskedTextBoxDATE1";
+            this.maskedTextBoxDATE1.Size = new System.Drawing.Size(87, 22);
+            this.maskedTextBoxDATE1.TabIndex = 0;
+            this.maskedTextBoxDATE1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBoxDATE1.ValidatingType = typeof(System.DateTime);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.maskedTextBoxDATE1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 93);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введите начальную дату:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(2, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(202, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Установить текущую дату и время";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дата:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox2.Controls.Add(this.maskedTextBoxDATE2);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(252, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(210, 93);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Введите конечную дату:";
+            // 
+            // maskedTextBoxDATE2
+            // 
+            this.maskedTextBoxDATE2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.maskedTextBoxDATE2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBoxDATE2.Location = new System.Drawing.Point(94, 27);
+            this.maskedTextBoxDATE2.Mask = "00/00/0000";
+            this.maskedTextBoxDATE2.Name = "maskedTextBoxDATE2";
+            this.maskedTextBoxDATE2.Size = new System.Drawing.Size(87, 22);
+            this.maskedTextBoxDATE2.TabIndex = 5;
+            this.maskedTextBoxDATE2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBoxDATE2.ValidatingType = typeof(System.DateTime);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(2, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(202, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Установить текущую дату и время";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Дата:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.groupBox3.Location = new System.Drawing.Point(491, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(115, 178);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Рассчитать";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 148);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(98, 20);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "В секундах";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 125);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(92, 20);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "В минутах";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 99);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(75, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "В часах";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 73);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "В днях";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "В месяцах";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "В годах";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(451, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(619, 202);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расчет времени ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxDATE1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxDATE2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCountsYearMonthDay
+{
+    public partial class Form1 : Form
+    {
+        CountTime countTime; // класс расчета времени 
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        // чек боксы текущей даты
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                maskedTextBoxDATE1.Text = DateTime.Now.ToShortDateString();
+            }
+            else maskedTextBoxDATE1.Clear();
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                maskedTextBoxDATE2.Text = DateTime.Now.ToShortDateString();
+            }
+            else maskedTextBoxDATE2.Clear();
+        }
+        // вход в зону кнопки
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.DarkKhaki;
+        }
+        // выход из зоны кнопки
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Silver;
+        }
+
+        /// кнопка рассчета
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = "";
+                DateTime dt1 = Convert.ToDateTime(maskedTextBoxDATE1.Text);
+                DateTime dt2 = Convert.ToDateTime(maskedTextBoxDATE2.Text);
+                countTime = new CountTime(dt1, dt2);
+                message += "Время между датами[";
+                if (radioButton1.Checked)
+                {
+
+                    int dorb = countTime.Days % 365;
+                    message += $"{Math.Abs(countTime.Years)}";
+                    if (dorb >= 180) message += ".5";
+                    message += $" год/лет]";
+                }
+                if (radioButton2.Checked)
+                {
+                    int dorb2 = countTime.Days % 30;
+                    message += $"{Math.Abs(countTime.Months)}";
+                    if (dorb2 >= 15) message += ".5";
+                    message += $" месяцев]";
+                }
+                if (radioButton3.Checked) message += $"{Math.Abs(countTime.Days)} дней]";
+                if (radioButton4.Checked) message += $"{Math.Abs(countTime.Hours)} часов]";
+                if (radioButton5.Checked) message += $"{Math.Abs(countTime.Minutes)} минут]";
+                if (radioButton6.Checked) message += $"{Math.Abs(countTime.Seconds)} секунд]";
+                MessageBox.Show(message, "Результат");
+            }
+            catch
+            {
+                MessageBox.Show("Заполните все поля дат!", "Внимание!", MessageBoxButtons.OK);
+            }
+        }
+
+    }
+
+    class CountTime
+    {
+        public DateTime datatime1 { get; set; }
+        public DateTime datatime2 { get; set; }
+        public int Years    { get   { return Days / 365; } }
+        public int Months   { get   { return this.Days / 30;  } }
+        public int Days     { get   { return getDays(); } }
+        public int Hours    { get   { return this.Days * 24;} }
+        public int Minutes  { get   { return this.Hours * 60; } }
+        public int Seconds  { get   { return this.Minutes * 60; } }
+        public CountTime(DateTime d1, DateTime d2)
+        {
+            if(d1 > d2)
+            {
+                this.datatime1 = d2;
+                this.datatime2 = d1;
+            }
+            else 
+            {
+                this.datatime1 = d1;
+                this.datatime2 = d2;
+            }
+        }
+        public int getDaysFromMonth(DateTime d)
+        {
+            int quantity1 = 0;
+            int m1 = 31, m2 = 28, m3 = 31, m4 = 30,
+                m5 = 31, m6 = 30, m7 = 31, m8 = 31,
+                m9 = 30, m10 = 31, m11 = 30, m12 = 31;
+            switch (d.Month)
+            {
+                case 1: quantity1 = d.Day; break;
+                case 2: quantity1 = m1 + d.Day; break;
+                case 3: quantity1 = m1 + m2 + d.Day; break;
+                case 4: quantity1 = m1 + m2 + m3 + d.Day; break;
+                case 5: quantity1 = m1 + m2 + m3 + m4 + d.Day; break;
+                case 6: quantity1 = m1 + m2 + m3 + m4 + m5 + d.Day; break;
+                case 7: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + d.Day; break;
+                case 8: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + d.Day; break;
+                case 9: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + d.Day; break;
+                case 10: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + d.Day; break;
+                case 11: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + d.Day; break;
+                case 12: quantity1 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + d.Day; break;
+            }
+            return quantity1;
+        }
+        public int getDays()
+        {
+            int leapY, QleapY = 0;
+            leapY = datatime1.Year;
+            while (leapY <= datatime2.Year)
+            {
+                if (leapY % 4 == 0 && leapY % 100 != 0 || leapY % 400 == 0)
+                {
+                    QleapY++;
+                }
+                leapY++;
+            }
+            int daysFromMouth1 = getDaysFromMonth(this.datatime1);
+            int daysFromMouth2 = getDaysFromMonth(this.datatime2);
+            if ((datatime1.Year % 4 == 0 && leapY % 100 != 0 || leapY % 400 == 0) && datatime1.Month > 2)
+            { QleapY--; }
+            if ((datatime2.Year % 4 == 0 && leapY % 100 != 0 || leapY % 400 == 0) && datatime2.Day <= 29 && datatime2.Month < 3)
+            { QleapY--; }
+            if (datatime1.Year == datatime2.Year)
+                    return (daysFromMouth2 - daysFromMouth1) + QleapY;
+                else
+                    return (datatime2.Year - datatime1.Year) * 365 + ((daysFromMouth2 - daysFromMouth1) + QleapY);
+        }
+    }
+
+
+}
